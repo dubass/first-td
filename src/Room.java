23171dcc33e23e1608c4275174c437444add4e16
dubass <terorist40@yandex.ru>
2016-08-22 12:52:05 +0300

    -add Block.java

--- src/Room.java
+++ src/Room.java
@@ -1,7 +1,30 @@
+import java.awt.Graphics;
 
 public class Room {
+	public int worldWidth = 10;
+	public int worldHeight = 6;
+	public int blockSize = 32;
+	
+	public Block[][] block;
 	
 	public Room(){
+		define();
+		
+	}
+	
+	public void define(){
+		block = new Block[worldWidth][worldHeight];
+		
+		for(int y=0;y<block.length;y++){
+			
+		}
+	}
+	
+	public void physic(){
+		
+	}
+	
+	public void draw(Graphics g){
 		
 	}
 
--- src/Screen.java
+++ src/Screen.java
@@ -10,27 +10,33 @@ public class Screen extends JPanel implements Runnable{
 	
 	public static boolean isFirst = true;
 	
+	public static Room room;
+	
 	public Screen(){
 		thread.start();
 	}
 	
 	public void define(){
-		
+		room = new Room();
 	}
 	
 	public void paintComponent(Graphics g){
 		if(isFirst){
 			define();
+			isFirst = false;
 		}
 		
 		g.clearRect(0, 0, getWidth(), getHeight());
+		room.draw(g);
 	}
 	
 	
-	public static int fpsFrame = 0, fps = 1000000;
+	
 	public void run(){
 		while(true){
-			if(!isFirst)
+			if(!isFirst){
+				room.physic();
+			}
 			repaint();
 			
 			try{
